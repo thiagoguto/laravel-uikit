57f52dced2001b7aea51c1c912de9ac554fa8f20
Thiago Augusto <thiagoaugusto31@gmail.com>
2019-02-02 21:31:18 -0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <!-- Styles -->
+        <link rel="stylesheet" href="/assets/css/app.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.20/js/uikit.min.js" 
+            integrity="sha256-2vL3vDc2j7XdT1qgHvWPSCmULPhJ6q0bLlPIgPXFQPI=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-rc.20/js/uikit-icons.min.js" 
+            integrity="sha256-eQPdtNjxSg+WO2RwUfv5Jb/ZSlCIfsH6umy+Q9Fpuk0=" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div  class="uk-container uk-container-small" >
+            <h1>Home page</h1>
+        </div>
+    </body>
+</html>
